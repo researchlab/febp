755350c581f5055b07d6aa386dba382ad47533ef
hong li <lihongitchina@gmail.com>
2020-09-07 14:43:13 +0800

    696. 计数二进制子串

--- js-practice/leetcode/test/string/696.count-binary-substrings.test.js
+++ js-practice/leetcode/test/string/696.count-binary-substrings.test.js
@@ -1,9 +1,17 @@
 import subStr from '../../code/string/696.count-binary-substrings';
 
+// test('subStr(00110011)',()=>{
+//     expect(subStr('00110011')).toEqual(['0011','01','1100','10','0011','01']);
+// });
+
+// test('subStr(10101',()=>{
+//     expect(subStr('10101')).toEqual(['10','01','10','01']);
+// });
+
 test('subStr(00110011)',()=>{
-    expect(subStr('00110011')).toEqual(['0011','01','1100','10','0011','01']);
+    expect(subStr('00110011')).toEqual(6);
 });
 
 test('subStr(10101',()=>{
-    expect(subStr('10101')).toEqual(['10','01','10','01']);
+    expect(subStr('10101')).toEqual(4);
 });
