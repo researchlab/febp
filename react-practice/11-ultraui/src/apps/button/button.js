f347cfa1f7eb523afcfcf0dae3fbbf19395024b3
hong li <lihongitchina@gmail.com>
2020-10-23 16:35:07 +0800

    update

--- react-practice/11-ultraui/src/apps/button/button.js
+++ react-practice/11-ultraui/src/apps/button/button.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Button, ButtonGroup, ButtonList } from '@huayun/ultraui';
+import {Button, ButtonGroup, ButtonList,Static } from '@huayun/ultraui';
 import './button.less';
 
 class UltraButton extends React.Component{
     render(){
         return (<div className="wrapper">
+            <Static value="默认" style={{backgroundColor:'red'}}/>
             <ButtonList className="m-b">
-            <Button type="primary" name="主按钮" />
+            <Button type="primary" className="test" name="主按钮" />
             <Button type="default" name="次按钮" />
             <Button type="dashed" name="虚线按钮" />
             <Button type="primary" icon="add">创建</Button>
